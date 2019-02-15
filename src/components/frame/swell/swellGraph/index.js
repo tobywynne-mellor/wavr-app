bf07040d9e6ada17b8092ecdee8f69e1fb1c66dc
Toby <tobytogs@hotmail.com>
2019-02-15 23:18:26 +0000

    initial swell diagram

--- src/components/frame/swell/swellGraph/index.js
+++ src/components/frame/swell/swellGraph/index.js
@@ -11,7 +11,29 @@ export default class swellGraph extends Component {
 	}
 
 	render() {
-		// return <ChartistGraph data={lineChartData} options={lineChartOptions} type={'Line'} />;
+		let lineChartData = {
+			labels: [1, 2, 3, 4, 5, 6, 7, 8],
+			series: [
+			  [5, 9, 7, 8, 5, 3, 5, 4]
+			]
+		  };
+
+		  let lineChartOptions = {
+			low: 0,
+			showArea: true,
+			showLine: false,
+			showPoint: false,
+			axisX: {
+				showLabel: false,
+				showGrid: false
+			},
+			axisY: {
+				showLabel: false,
+				showGrid: false
+			}
+		  };
+		return <ChartistGraph data={lineChartData} options={lineChartOptions} type={'Line'}/>;
+
 	}
 
 	componentDidMount() {
