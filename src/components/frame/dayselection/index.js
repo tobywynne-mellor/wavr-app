3246411522101a4ebf2ffa6bb3527eed7be80d2a
Oscar Davis <administrator@oscars-mbp.lan>
2019-02-21 13:42:31 +0000

    Funcitoning radio buttons to select day, changes state from 0-2

--- src/components/frame/dayselection/index.js
+++ src/components/frame/dayselection/index.js
@@ -1,5 +1,3 @@
-//testing commit
-
 // import preact
 import { h, render, Component } from 'preact';
 // import stylesheets for ipad & button
@@ -10,6 +8,22 @@ export default class DaySelection extends Component {
 
 	constructor(props){
 		super(props);
+		this.handleChange = this.handleChange.bind(this);
+		this.state = {
+	      day: ''
+	  };
+	}
+
+
+	convertTimestamp(str) {
+		let date = new Date(parseInt(str,10)*1000);
+		return date.getHours();
+	}
+
+	handleChange(event) {
+	  this.setState({
+	    day: event.target.value
+	  });
 	}
 
 	//returns 0-2
@@ -18,10 +32,40 @@ export default class DaySelection extends Component {
 
 	render() {
 		return (
-            <div>
-                <p>Day Selection</p>
-								<p>testing commit</p>
-            </div>
+        <div>
+						<ul>
+							<li>
+								<label>
+									<input
+										type="radio"
+										value="0"
+										checked={this.state.day === "0"}
+										onChange={this.handleChange}
+									/>Today
+								</label>
+							</li>
+							<li>
+								<label>
+									<input
+										type="radio"
+										value="1"
+										checked={this.state.day === "1"}
+										onChange={this.handleChange}
+									/>Tomorrow
+								</label>
+							</li>
+							<li>
+								<label>
+									<input
+										type="radio"
+										value="2"
+										checked={this.state.day === "2"}
+										onChange={this.handleChange}
+									/>Next Day
+								</label>
+							</li>
+						</ul>
+        </div>
 		);
 	}
 }
