ddbaedc622c96c8c7eaf4ad4971ee1608364c10b
Toby <tobytogs@hotmail.com>
2019-02-23 15:00:55 +0000

    created general style sheet and imported to all component style sheets

--- src/components/frame/slider/index.js
+++ src/components/frame/slider/index.js
@@ -23,7 +23,7 @@ export default class Slider extends Component {
 		return (
 			<div class="container">
 				<hr/>
-				<h5>Slider</h5>
+				<h5 class = { style.fontColour } >Slider</h5>
         <input type="range" min="0" max="21" value={this.props.time} step = "3" id="slider" oninput={this.handleChange}/>
 				<p>Time: {this.props.timeText}</p>
       	<SurfRating rating = {this.props.rating}/>
