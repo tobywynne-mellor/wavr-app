3bc404e772782a14a02bdce4a3511991592d9396
Toby <tobytogs@hotmail.com>
2019-02-25 22:52:10 +0000

    tide

--- src/components/frame/tide/index.js
+++ src/components/frame/tide/index.js
@@ -15,6 +15,10 @@ export default class Tide extends Component {
 		return (
             <div class = { style.tide }>
                 <Title text="Tide"/>
+				<svg class={style.svg} viewBox="0 0 350 90">
+					<path d="M"></path>
+					<rect x="10" y="20" width="330" height="50" class={style.backdrop}></rect>
+				</svg>
             </div>
 		);
 	}
