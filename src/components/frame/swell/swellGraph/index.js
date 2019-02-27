f0c03aaa625e91a5d36be7493729ea65d209e4e5
Toby <tobytogs@hotmail.com>
2019-02-27 16:16:28 +0000

    remove react chartist

--- src/components/frame/swell/swellGraph/index.js
+++ src/components/frame/swell/swellGraph/index.js
@@ -3,7 +3,6 @@ import { h, render, Component } from 'preact';
 // import stylesheets for ipad & button
 import style from './style';
 import { isBoolean } from 'util';
-import ChartistGraph from 'react-chartist';
 
 export default class swellGraph extends Component {
 	constructor(props) {
