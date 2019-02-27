251089201066d1c89a1045474b6c3775d5c591e2
lorexsan <lorenzosani97@gmail.com>
2019-02-27 20:34:44 +0000

    Swell diagram

--- src/components/frame/swell/index.js
+++ src/components/frame/swell/index.js
@@ -4,7 +4,7 @@ import { h, render, Component } from 'preact';
 import style from './style';
 import { isBoolean } from 'util';
 import SwellBoxes from './swellBoxes';
-// import SwellGraph from './swellGraph';
+import SwellGraph from './swellGraph';
 import Title from '../title';
 
 export default class Swell extends Component {
@@ -15,13 +15,11 @@ export default class Swell extends Component {
 
 	render() {
 		return (
-            <div class = { style.swell }>
-
+      <div class = { style.swell }>
 				<Title text = "Swell"/>
-				{/* for tool tip: https://react-bootstrap.github.io/components/overlays/#tooltips */}
 				<SwellBoxes swell = { this.props.swell} index = {this.props.index}/>
-				{/* <SwellGraph swell = { this.props.swell} index = {this.props.index}/> */}
-            </div>
+				<SwellGraph swell = { this.props.swell} index = {this.props.index}/>
+      </div>
 		);
 	}
 }
---
+++ src/components/frame/swell/swellGraph/index.js
@@ -0,0 +1,59 @@
+// import preact
+import { h, render, Component } from 'preact';
+// import stylesheets for ipad & button
+import style from './style';
+import { isBoolean } from 'util';
+
+export default class swellGraph extends Component {
+
+	constructor(props) {
+		super(props);
+		this.createWave = this.createWave.bind(this)
+		this.state = {
+			path : ""
+		}
+	}
+
+	createWave() {
+		let HEIGHT = 2.5 //this.props.swell.primary.height[this.props.index] //between 0 and 5
+		let FREQUENCE = 15 //this.props.swell.primary.period[this.props.index] //between 5 and 25
+
+		let xs = []
+		for(var i=0; i<=500; i++) {
+			xs.push(i)
+		}
+		let points = xs.map( x => {
+			//wave = xHeight + waveHeight * waveFrequence
+			let xHeight = (6-HEIGHT)*30
+			let waveHeight = FREQUENCE+10
+			let waveFrequence = Math.sin(x/((25-FREQUENCE)*3))
+			let y = xHeight + waveHeight * waveFrequence
+
+			return [x, y]
+		})
+
+		let path = "M" + points.map( p => {
+			return p[0] + "," + p[1]
+		}).join(" L")
+
+	  let pathFinal = path + " L500,250 L0,250 L0," + points[0][1]
+
+		this.setState({
+	    path: pathFinal
+	  });
+	}
+
+	render() {
+		return (
+			<div class={style.cont}>
+				<svg viewBox = "0 0 500 250" class = { style.swellSvg }>
+					<path class={style.swellPath} d={this.state.path}></path>
+				</svg>
+			</div>
+		);
+	}
+
+	componentDidMount() {
+		this.createWave()
+	}
+}
