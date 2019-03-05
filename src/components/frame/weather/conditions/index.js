d6a44e3a4d2ae21d3b1ec3266ceb31f3006de45f
Oscar Davis <administrator@Oscars-MacBook-Pro.local>
2019-03-05 17:19:58 +0000

    icons working

--- src/components/frame/weather/conditions/index.js
+++ src/components/frame/weather/conditions/index.js
@@ -3,7 +3,6 @@ import { h, render, Component } from 'preact';
 // import stylesheets for ipad & button
 import style from './style';
 import { isBoolean } from 'util';
-import sun from './1.png';
 
 export default class Conditions extends Component {
 
@@ -14,10 +13,9 @@ export default class Conditions extends Component {
 	render() {
 		return (
             <div>
+								<img src={this.iconString()} class={style.image}> </img>
                 <p>temp: { this.props.temperature }</p>
                 <p>chill: { this.props.chill }</p>
-
-                <img>src={sun}</img>
             </div>
 		);
 	}
