e75405daffe9db8e6e535e34cbb763ec53ccd963
Oscar Davis <administrator@oscars-mbp.lan>
2019-03-06 00:19:57 +0000

    changed ratio of components for better fit + made wind description text

--- src/components/frame/weather/conditions/index.js
+++ src/components/frame/weather/conditions/index.js
@@ -13,10 +13,11 @@ export default class Wind extends Component {
 	render() {
 		return (
 			<div class={style.grid}>
-			<div class={style.subGrid}>
+			<div class={style.subGridA}>
 				<img src='./../../../assets/weatherIcons/thermometer.png' class={style.smallImage}></img><p>{ this.props.temperature } °C</p>
 				<img src='./../../../assets/weatherIcons/chill.png' class={style.smallImage}></img> <p>{ this.props.chill } °C</p>
 			</div>
+			 <div class={style.subGridB}>
 				<svg class={style.dial} viewBox='0 0 200 200'>
 					<ellipse class={style.backCircle} ry="80" rx="80" cy="100" cx="100" stroke-width="3" stroke="#ffffff" fill="#000" />
 					<path class={style.bg} transform="translate (0,-13)" d="m40.32092,163.95455a79.14646,78.68765 0 1 1 121.21743,0" fill="none" stroke-width="11" />
@@ -25,11 +26,40 @@ export default class Wind extends Component {
 					<text class={style.speedText} font-family="Sans-serif" font-size="18" id="speedText" stroke="#ff0000" stroke-width="0" text-anchor="middle" x="100" y="130">{this.props.speed} mph</text>
 					<path class={style.pointer} transform={this.rotatePointer()} d="M100,180 L110,150 L90,150 z" />
 				</svg>
+				<p id={style.windText}>{this.windText()}</p>
+			</div>
 				<div>
-					<img src={this.iconString()} class={style.image}> </img>
+					<img src={this.iconString()} class={style.image}></img>
 				</div>
 			</div>
 		);
+	}
+
+	windText(){
+		//direction text
+		var direction;
+		//thurso and newquay off-shore wind is south-east
+		if(this.props.direction >= 180 && this.props.direction <=360){
+			direction = 'off-shore';
+		}
+		else{
+			direction = 'on-shore';
+		}
+
+		//strength text
+		var strength;
+		if(this.props.speed < 4){
+			strength = 'weak';
+		}
+		else if(this.props.speed >= 4 && this.props.speed <= 20){
+			strength = 'medium';
+		}
+		else{
+			strength = 'strong';
+		}
+
+		//return string
+		return '' + strength + ' ' + direction + ' wind';
 	}
 
 	iconString(){
