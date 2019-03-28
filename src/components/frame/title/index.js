305380304df7fdfe9436d6b6d0e61d3cd7c1275c
oscartdavis@gmail.com <od302@itl085.student.eecs.qmul.ac.uk>
2019-03-28 15:40:39 +0000

    tooltip icons added

--- src/components/frame/title/index.js
+++ src/components/frame/title/index.js
@@ -12,6 +12,14 @@ export default class Title extends Component {
 
 	render() {
 		//returns uppercase with title style
-		return <div class={style.title}>{this.props.text.toUpperCase()}</div>;
+		return(
+		<div class={style.box}>
+			<div class={style.title}>{this.props.text.toUpperCase()}</div>
+				<svg class={style.tipIcon} width="20" height="20" viewBox="0 0 100 100">
+			  	<ellipse cx="50" cy="50" fill="none" id="circle" rx="40" ry="40" stroke="#f3f3f3" stroke-width="10"/>
+			  	<text fill="#f3f3f3" font-family="sans-serif" font-weight="bold" font-size="72" id="text" stroke="#f3f3f3" stroke-width="0" text-anchor="middle" x="50" y="74">?</text>
+				</svg>
+		</div>
+		);
 	}
 }
